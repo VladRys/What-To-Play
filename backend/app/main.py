9e8f3a6a2ac44fd53eb781aa708d1e83b3112ec5
n1ghtc0ree <sashahalimonen123@gmail.com>
2026-04-20 20:21:06 +0300

    merge: actualized back-end for front-end

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
+import logging
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+
+from fastapi.responses import JSONResponse
+
 from backend.app.routers.games import router as games_router
 from backend.app.routers.steam import router as steam_router
-import logging
+from backend.app.exceptions import UnknownVibeException
+from backend.app.config import config
 
 app = FastAPI()
 
@@ -25,3 +30,21 @@ def home():
     """Test route to check if API is working"""
     logger.info("Home route accessed")
     return {"message": "API is working"}
+
+# --- Global Exceptions Handler ---
+@app.exception_handler(Exception)
+async def general_exception_handler(request: Request, exc: Exception):
+    logger = request.state.logger  
+    logger.error(f"Unexpected error: {str(exc)}", exc_info=True)
+    
+    return {
+        "message": "Internal server error", 
+        "status": 500
+    }
+
+@app.exception_handler(UnknownVibeException)
+async def unknown_vibe_handler(request: Request, exc: UnknownVibeException):
+    return JSONResponse(
+            content=f"Uknown vibe, no games was returned. Use {config.VIBES_MAP.keys()}.",
+            status_code=404
+        )
